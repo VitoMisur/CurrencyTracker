9261fbff43874cd2cc620b509e30a3218036b35a
Vitaliy Misurenko <vitaliy.misurenko@gmail.com>
2023-05-26 20:37:26 +0200

    update room and cleanup entity with model

--- app/src/main/java/com/vito/misur/currencytracker/database/entity/Currency.kt
+++ app/src/main/java/com/vito/misur/currencytracker/database/entity/Currency.kt
@@ -1,24 +1,16 @@
 package com.vito.misur.currencytracker.database.entity
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.IgnoredOnParcel
-import kotlinx.android.parcel.Parcelize
 
-@Entity(
-    tableName = "supported_currency"
-    /*, indices = [Index(value = ["name"], unique = true)]*/
-)
-@Parcelize
+@Entity(tableName = "supported_currency")
 data class Currency(
     @field:SerializedName("symbol") @ColumnInfo(name = "symbol") val symbol: String,
     @field:SerializedName("name") @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "is_main_currency") val isMainCurrency: Boolean = false
-) : Parcelable {
-    @IgnoredOnParcel
+) {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     var currencyId: Long = 0
--- app/src/main/java/com/vito/misur/currencytracker/view/data/CurrencyItem.kt
+++ app/src/main/java/com/vito/misur/currencytracker/view/data/CurrencyItem.kt
@@ -1,12 +1,8 @@
 package com.vito.misur.currencytracker.view.data
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class CurrencyItem(
     val currencyId: Long,
     val symbol: String,
     val name: String,
     val isMainCurrency: Boolean
-) : Parcelable
+)
